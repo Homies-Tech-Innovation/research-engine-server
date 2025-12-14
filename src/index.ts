99106a3f6162295bd64dafcc57ba14f6dd3f30f4
aditsuru <me@aditsuru.com>
2025-12-14 17:08:08 +0530

    feat: Unhandled rejection handlers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
+import type { Server } from 'node:http';
 import { config } from '@config';
 import { logger } from '@utils';
 import app from '@/app';
 
-app.listen(config.env.PORT, () => {
-  logger.info(`\x1b[92m✔ server running at\x1b[0m http://localhost:${config.env.PORT}`);
+// Start the app
+let server: Server;
+const init = async () => {
+  server = app.listen(config.env.PORT, () => {
+    logger.info(`\x1b[92m✔ server running at\x1b[0m http://localhost:${config.env.PORT}`);
+  });
+};
+
+init();
+
+// Handle uncaught errors
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+// Handle SIGTERM signal
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received. Shutting down.');
+  if (server) {
+    server.close();
+  }
 });
+
+// Helper function
+function unexpectedErrorHandler(error: unknown) {
+  logger.error(error, 'Unexpected Error. Shutting down.');
+
+  // Kill the server properly
+  if (server) {
+    server.close(() => process.exit(1));
+  } else {
+    process.exit(1);
+  }
+}
